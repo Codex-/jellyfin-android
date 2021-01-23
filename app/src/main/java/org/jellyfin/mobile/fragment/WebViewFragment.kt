77088e3d871afa5115f40962fc071c855ce4e04e
Niels van Velzen <git@ndat.nl>
2021-01-23 13:10:53 +0100

    Switch to new ApiClient for non-player related classes

--- app/src/main/java/org/jellyfin/mobile/fragment/WebViewFragment.kt
+++ app/src/main/java/org/jellyfin/mobile/fragment/WebViewFragment.kt
@@ -24,7 +24,6 @@ import androidx.webkit.WebResourceErrorCompat
 import androidx.webkit.WebViewClientCompat
 import androidx.webkit.WebViewFeature
 import kotlinx.coroutines.launch
-import org.jellyfin.apiclient.interaction.ApiClient
 import org.jellyfin.mobile.MainActivity
 import org.jellyfin.mobile.R
 import org.jellyfin.mobile.bridge.ExternalPlayer
@@ -49,7 +48,6 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 class WebViewFragment : Fragment(), NativePlayerHost {
-    val apiClient: ApiClient by inject()
     private val serverController: ServerController by inject()
     private val webappFunctionChannel: WebappFunctionChannel by inject()
     private val externalPlayer by lazy { ExternalPlayer(this) }
